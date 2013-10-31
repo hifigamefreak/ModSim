850c36b8c417e185fddd9a0d872fc61ea9e55ee1
Joost Hekman <Joost.Hekman@student.uva.nl>
2013-10-31 10:36:23 +0100

    Added some cases for getting Inf & NaN.

--- operations.c
+++ operations.c
@@ -13,11 +13,15 @@ int main(int argc, char *argv[]) {
    /* Generate Inf */	
    printf("1.0 / 0.0 = %f\n", 1.0 / 0.0);
    printf("-1.0 / 0.0 = %f\n", -1.0 / 0.0);
-
+   printf("log(0) = %f\n", log(0));
+   printf("sqrt(-1) = %f\n", sqrt(-1));
+	
    /* Generate NaN */
    printf("0.0 / 0.0 = %f\n", 0.0 / 0.0);
    printf("Inf / Inf = %f\n", INFINITY / INFINITY);
-
+   printf("sin(2)^-1 = %f\n", asin(2));
+   printf("cos(-2)^-1 = %f\n", acos(-2));
+ 
    /* Generate singed zero */
    printf("1.0 / -Inf = %f\n", 1.0 / -INFINITY);
    printf("1.0 / Inf = %f\n", 1.0 / INFINITY);
